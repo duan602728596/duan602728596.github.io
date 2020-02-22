04ad610f97114b0b8151099bf285eb5e1a1c6691
段昊辰 <duanhaochen@126.com>
2020-02-22 18:49:48 +0800

    refactor: 调整函数名称

--- src/script/webpackEntry/vendor.js
+++ src/script/webpackEntry/vendor.js
@@ -1,8 +1,10 @@
 import codeHighlight from '../vendor/codeHighlight';
 import goToTopInit from '../vendor/goToTop';
 
-if (window?.__INITIAL_STATE__?.codeHighlightLength) {
-  codeHighlight(window?.__INITIAL_STATE__?.codeHighlightLength);
+const { __INITIAL_STATE__: initialState } = window;
+
+if (initialState?.codeHighlightLength) {
+  codeHighlight(initialState?.codeHighlightLength);
 }
 
 goToTopInit();
