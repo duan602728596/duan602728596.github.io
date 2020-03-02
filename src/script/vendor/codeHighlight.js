bc7809f09ca7935466473f9cbb9c107a149e7cd6
段昊辰 <duanhaochen@126.com>
2020-03-02 22:53:15 +0800

    feat: 使用SVG，让渐变的背景也能动起来

--- src/script/vendor/codeHighlight.js
+++ src/script/vendor/codeHighlight.js
@@ -1,9 +1,13 @@
 import hljs from 'highlight.js/lib/highlight';
 import javascript from 'highlight.js/lib/languages/javascript';
 import typescript from 'highlight.js/lib/languages/typescript';
+import xml from 'highlight.js/lib/languages/xml';
+import css from 'highlight.js/lib/languages/css';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('css', css);
 
 /* 代码高亮 */
 function codeHighlight(len) {
