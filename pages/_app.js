61ac60d212079c301a38b4be71d9c517cc92f447
段昊辰 <duanhaochen@126.com>
2020-06-16 09:38:47 +0800

    perf: 样式兼容移动端

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ export default function(props) {
     <Fragment>
       <Head>
         <meta name="renderer" content="webkit" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta httpEquiv="Window-target" content="_top" />
         <meta name="author" content="段昊辰, duanhaochen@126.com" />
         <meta name="copyright" content="段昊辰, duanhaochen@126.com" />
