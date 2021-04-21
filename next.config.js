d89e0e6693403e21634a63261b325dc42daa870e
段昊辰 <duanhaochen@126.com>
2021-04-21 17:43:57 +0800

    refactor: 提取到通用函数

--- next.config.js
+++ next.config.js
@@ -1,82 +1,9 @@
 const process = require('process');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const Fiber = require('fibers');
+const { lessLoader, sassLoader } = require('./utils/css');
 
 const isDevelopment = process.env.NODE_ENV === 'development';
-
-function cssLoaderModeFunc(resourcePath) {
-  if (/(pure\.(css|less|sass|scss|styl(us)?))/i.test(resourcePath)) {
-    return 'pure';
-  }
-
-  if (/(node_modules|global\.(css|less|sass|scss|styl(us)?))/i.test(resourcePath)) {
-    return 'global';
-  }
-
-  return 'local';
-}
-
-function cssLoader(config, options) {
-  return {
-    loader: 'css-loader',
-    options: {
-      modules: {
-        mode: cssLoaderModeFunc,
-        localIdentName: isDevelopment ? '[path][name]__[local]___[hash:base64:6]' : '_[hash:base64:6]',
-        exportOnlyLocals: options.isServer
-      }
-    }
-  };
-}
-
-/* 添加less-loader */
-function lessLoader(config, options) {
-  const use = [
-    cssLoader(config, options),
-    {
-      loader: 'less-loader',
-      options: {
-        lessOptions: {
-          javascriptEnabled: true
-        }
-      }
-    }
-  ];
-
-  config.module.rules.push({
-    test: /^.*\.(le|c)ss$/i,
-    use
-  });
-
-  if (!options.isServer) {
-    use.unshift(isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader);
-  }
-}
-
-/* 添加sass-loader */
-function sassLoader(config, options) {
-  const use = [
-    cssLoader(config, options),
-    {
-      loader: 'sass-loader',
-      options: {
-        sassOptions: {
-          fiber: Fiber
-        }
-      }
-    }
-  ];
-
-  config.module.rules.push({
-    test: /^.*\.s(a|c)ss$/i,
-    use
-  });
-
-  if (!options.isServer) {
-    use.unshift(isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader);
-  }
-}
 
 module.exports = function() {
   return {
---
+++ utils/css.js
@@ -0,0 +1,83 @@
+const process = require('process');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Fiber = require('fibers');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+/* css-loader mode函数 */
+function cssLoaderModeFunc(resourcePath) {
+  if (/(pure\.(css|less|sass|scss|styl(us)?))/i.test(resourcePath)) {
+    return 'pure';
+  }
+
+  if (/(node_modules|global\.(css|less|sass|scss|styl(us)?))/i.test(resourcePath)) {
+    return 'global';
+  }
+
+  return 'local';
+}
+
+/* css-loader配置 */
+function cssLoader(config, options) {
+  return {
+    loader: 'css-loader',
+    options: {
+      modules: {
+        mode: cssLoaderModeFunc,
+        localIdentName: isDevelopment ? '[path][name]__[local]___[hash:base64:6]' : '_[hash:base64:6]',
+        exportOnlyLocals: options.isServer
+      }
+    }
+  };
+}
+
+/* 添加less-loader */
+function lessLoader(config, options) {
+  const use = [
+    cssLoader(config, options),
+    {
+      loader: 'less-loader',
+      options: {
+        lessOptions: {
+          javascriptEnabled: true
+        }
+      }
+    }
+  ];
+
+  config.module.rules.push({
+    test: /^.*\.(le|c)ss$/i,
+    use
+  });
+
+  if (!options.isServer) {
+    use.unshift(isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader);
+  }
+}
+
+/* 添加sass-loader */
+function sassLoader(config, options) {
+  const use = [
+    cssLoader(config, options),
+    {
+      loader: 'sass-loader',
+      options: {
+        sassOptions: {
+          fiber: Fiber
+        }
+      }
+    }
+  ];
+
+  config.module.rules.push({
+    test: /^.*\.s(a|c)ss$/i,
+    use
+  });
+
+  if (!options.isServer) {
+    use.unshift(isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader);
+  }
+}
+
+exports.lessLoader = lessLoader;
+exports.sassLoader = sassLoader;
