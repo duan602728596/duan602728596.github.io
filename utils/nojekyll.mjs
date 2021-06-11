2114a854a5f2ec11d67deed2f8933a0703c41248
段昊辰 <duanhaochen@126.com>
2021-06-11 18:13:26 +0800

    fix: 修复mjs下解析__dirname的错误

--- utils/nojekyll.mjs
+++ utils/nojekyll.mjs
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
+import { metaHelper } from '@sweet-milktea/utils';
 
+const { __dirname } = metaHelper(import.meta.url);
 const nojekyllFile = path.join(__dirname, '../out/.nojekyll');
 
 await fs.promises.writeFile(nojekyllFile, '', {
