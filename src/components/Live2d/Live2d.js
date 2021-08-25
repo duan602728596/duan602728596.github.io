7a258270b0887f3a240c30091ea6f1c82427c9f0
段昊辰 <duanhaochen@126.com>
2021-08-25 17:15:44 +0800

    perf: 优化live2d在移动端盖住链接的问题

--- src/components/Live2d/Live2d.js
+++ src/components/Live2d/Live2d.js
@@ -1,7 +1,18 @@
-import { useEffect, useRef } from 'react';
+import { Fragment, useEffect, useRef } from 'react';
 import { Button, Tooltip } from 'antd';
 import { SettingOutlined as IconSettingOutlined } from '@ant-design/icons';
 import style from './live2d.sass';
+
+/* 判断是否为移动端 */
+function isMobile() {
+  if (typeof window === 'object') {
+    const ua = window.navigator.userAgent;
+
+    return /iphone|ipad|ipod|ios|android/i.test(ua);
+  } else {
+    return false;
+  }
+}
 
 /* 界面展示live2d */
 function Live2d(props) {
@@ -26,18 +37,21 @@ function Live2d(props) {
   }, []);
 
   return (
-    <div className={ style.root }>
-      <canvas ref={ canvasRef } className={ style.canvas } width={ 230 } height={ 230 } />
-      <Tooltip title="切换舰娘" placement="right">
-        <Button className={ style.tapBtn }
-          type="primary"
-          size="small"
-          icon={ <IconSettingOutlined /> }
-          shape="circle"
-          onClick={ handleSwitchClick }
-        />
-      </Tooltip>
-    </div>
+    <Fragment>
+      <div className={ style.root }>
+        <canvas ref={ canvasRef } className={ style.canvas } width={ 230 } height={ 230 } />
+        <Tooltip title="切换舰娘" placement="right">
+          <Button className={ style.tapBtn }
+            type="primary"
+            size="small"
+            icon={ <IconSettingOutlined /> }
+            shape="circle"
+            onClick={ handleSwitchClick }
+          />
+        </Tooltip>
+      </div>
+      { isMobile() ? <div className={ style.mobileSpace } /> : null }
+    </Fragment>
   );
 }
 
