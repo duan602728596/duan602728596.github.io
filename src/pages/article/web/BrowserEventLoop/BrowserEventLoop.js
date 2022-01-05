6d12cf0d1784e8391e1fe8614a9a83c0800ccd8e
段昊辰 <duanhaochen@126.com>
2022-01-05 10:00:09 +0800

    refactor: 补充定时器的说明

--- src/pages/article/web/BrowserEventLoop/BrowserEventLoop.js
+++ src/pages/article/web/BrowserEventLoop/BrowserEventLoop.js
@@ -31,12 +31,30 @@ function BrowserEventLoop(props) {
       <PromiseRunExample />
       <Paragraph>输出：</Paragraph>
       <HighLight>{ '1\n2\n5\n3\n6\n4' }</HighLight>
+      <Title level={ 2 }>setTimeout和setInterval</Title>
+      <Paragraph>
+        setTimeout和setInterval的运行机制是，将指定的代码移出本次执行，等到下一轮Event Loop时，再检查是否到了指定时间。
+        如果到了，就执行对应的代码；如果不到，就等到再下一轮Event Loop时重新判断。
+        这意味着，setTimeout指定的代码，必须等到本次执行的所有代码都执行完，才会执行。
+      </Paragraph>
+      <Paragraph>
+        零延迟（Zero delay）并不是意味着回调会立即执行。在零延迟调用setTimeout时，其并不是过了给定的时间间隔后就马上执行回调函数。
+        其等待的时间基于队列里正在等待的消息数量。也就是说，setTimeout()只是将事件插入了任务队列，必须等到当前代码（执行栈）执行完，
+        主线程才会去执行它指定的回调函数。要是当前代码耗时很长，有可能要等很久，所以并没有办法保证回调函数一定会在setTimeout()指定的时间执行。
+      </Paragraph>
       <Alert type="info"
         message={ [
           '参考地址：',
-          <a key="link" href="https://febook.hzfe.org/awesome-interview/book3/browser-event-loop" target="_blank" rel="noopener noreferrer">
-            https://febook.hzfe.org/awesome-interview/book3/browser-event-loop
-          </a>
+          <div key="febook">
+            <a href="https://febook.hzfe.org/awesome-interview/book3/browser-event-loop" target="_blank" rel="noopener noreferrer">
+              https://febook.hzfe.org/awesome-interview/book3/browser-event-loop
+            </a>
+          </div>,
+          <div key="jianshu">
+            <a href="https://www.jianshu.com/p/73164966c6fe" target="_blank" rel="noopener noreferrer">
+              https://www.jianshu.com/p/73164966c6fe
+            </a>
+          </div>
         ] }
       />
     </ArticleLayout>
