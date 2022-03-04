2e66e8b5b3880f66c21380ebb16bbe5517771db3
duanhaochen <duanhaochen@126.com>
2022-03-04 11:53:50 +0800

    feat: 支持https的开发环境

---
+++ devServer.mjs
@@ -0,0 +1,30 @@
+import https from 'node:https';
+import * as fs from 'node:fs';
+import path from 'node:path';
+import url from 'node:url';
+import next from 'next';
+import { metaHelper } from '@sweet-milktea/utils';
+
+const { __dirname } = metaHelper(import.meta.url);
+const app = next({ dev: true, port: 5057 });
+const handle = app.getRequestHandler();
+
+const httpsConfig = {
+  key: await fs.promises.readFile(path.join(__dirname, 'dev.key')),
+  cert: await fs.promises.readFile(path.join(__dirname, 'dev.crt'))
+};
+
+await app.prepare();
+https.createServer(httpsConfig, async function(req, res) {
+  try {
+    const parsedUrl = url.parse(req.url, true);
+
+    await handle(req, res, parsedUrl);
+  } catch (err) {
+    console.error(err);
+    res.statusCode = 500;
+    res.end(err);
+  }
+}).listen(5057, (err) => {
+  if (err) throw err;
+});
