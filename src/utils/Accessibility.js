ea581e25eea97c97f65cb765175d3064ef7e2eef
段昊辰 <duanhaochen@126.com>
2022-09-18 23:19:38 +0800

    perf: 优化无障碍的脚本

--- src/utils/Accessibility.js
+++ src/utils/Accessibility.js
@@ -1,21 +1,37 @@
+let inAccessibilityStatus = false;
+
+// 鼠标监听事件
+function handleAccessibilityMousedown(event) {
+  if (event.isTrusted) {
+    inAccessibilityStatus = false;
+    document.body.classList.remove('accessibility');
+    document.removeEventListener('mousedown', handleAccessibilityMousedown);
+  }
+}
+
+// 键盘监听事件
+function handleAccessibilityKeydown(event) {
+  // Tab键事件
+  if (inAccessibilityStatus === false && (event.keyCode === 9 || event.code === 'Tab')) {
+    inAccessibilityStatus = true;
+    document.body.classList.add('accessibility');
+    document.addEventListener('mousedown', handleAccessibilityMousedown);
+  }
+
+  // 回车键事件
+  if (
+    inAccessibilityStatus
+    && (event.keyCode === 13 || event.code === 'Enter')
+    && document.activeElement !== document.body
+  ) {
+    const tabIndex = document.activeElement.getAttribute('tabindex');
+
+    if (!(tabIndex === undefined || tabIndex === null)) {
+      document.activeElement.click();
+    }
+  }
+}
+
 if (typeof document === 'object') {
-  document.addEventListener('keydown', function(event) {
-    if ((event.keyCode === 13 || event.code === 'Enter') && document.activeElement !== document.body) {
-      const tabIndex = document.activeElement.getAttribute('tabindex');
-
-      if (!(tabIndex === undefined || tabIndex === null)) {
-        document.activeElement.click();
-      }
-    }
-
-    if ((event.keyCode === 9 || event.code === 'Tab')) {
-      document.body.classList.add('accessibility');
-    }
-  });
-
-  document.addEventListener('mousedown', function(event) {
-    if (event.isTrusted) {
-      document.body.classList.remove('accessibility');
-    }
-  });
+  document.addEventListener('keydown', handleAccessibilityKeydown);
 }
