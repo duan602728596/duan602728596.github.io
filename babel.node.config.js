cafcbc32276a1a09e612f0a195c842c950b308f3
段昊辰 <duanhaochen@126.com>
2023-12-16 21:46:11 +0800

    build: 修改babel的编译配置

--- babel.node.config.js
+++ babel.node.config.js
@@ -1,19 +1,9 @@
-const process = require('node:process');
-
-const isDev = process.env.NODE_ENV === 'development';
-
 module.exports = function(a) {
   a.cache(true);
 
   return {
-    presets: [['@sweet-milktea/babel-preset-sweet', {
-      env: {
-        ecmascript: true,
-        targets: ['node 20']
-      }
-    }]],
-    plugins: [
-      !isDev && ['transform-react-remove-prop-types', { mode: 'remove', removeImport: true }]
-    ].filter(Boolean)
+    presets: [['@babel/preset-react', {
+      runtime: 'automatic'
+    }]]
   };
 };
