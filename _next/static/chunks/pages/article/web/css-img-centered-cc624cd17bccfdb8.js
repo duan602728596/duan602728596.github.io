a8cc48f09d774de8ec1f5da94e3a5e3ac8105ce0
duan602728596 <duanhaochen@126.com>
2024-02-27 10:53:42 +0000

    Deploying to main from @ duan602728596/duan602728596.github.io@ba4eb7fa14b2305e0ed0704e1a8ef9374fd4901a 🚀

---
+++ _next/static/chunks/pages/article/web/css-img-centered-cc624cd17bccfdb8.js
@@ -0,0 +1,23 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6369],{2742:function(e,i,n){(window.__NEXT_P=window.__NEXT_P||[]).push(["/article/web/css-img-centered",function(){return n(1196)}])},1196:function(e,i,n){"use strict";n.r(i),n.d(i,{default:function(){return h}});var s=n(85893),t=n(67294),c=n(9008),a=n.n(c),r=n(76315),l=n(98642),o=n.n(l),d=n(30673),m=n(75749),u=n(90791);let{Title:g,Paragraph:p}=r.default;var x=function(e){return(0,s.jsxs)(d.Z,{children:[(0,s.jsx)(g,{children:"使用css让图片自适应居中"}),(0,s.jsx)(p,{children:"通过CSS，让图片自适应居中，效果如下："}),(0,s.jsx)(u.Z,{className:o().picture,imageClassName:o().image,avifSrc:"/images/css-img-centered/31.avif",webpSrc:"/images/css-img-centered/31.webp",src:"/images/css-img-centered/31.jpg",alt:"魔法少女小圆"}),(0,s.jsx)(u.Z,{className:o().picture,imageClassName:o().image,avifSrc:"/images/css-img-centered/32.avif",webpSrc:"/images/css-img-centered/32.webp",src:"/images/css-img-centered/32.png",alt:"阿米莉亚\xb7华生"}),(0,s.jsx)(g,{level:2,children:"代码演示"}),(0,s.jsx)(p,{children:"实现这种特效有两种方式。"}),(0,s.jsx)(g,{level:3,children:"第一种"}),(0,s.jsx)(m.Z,{type:"css",children:`.image-box {
+  display: flex;
+  width: 500px;
+  height: 400px;
+}
+
+.image {
+  display: block;
+  margin: auto;
+  max-width: 100%;
+  max-height: 100%;
+}`}),(0,s.jsx)(g,{level:3,children:"第二种"}),(0,s.jsx)(m.Z,{type:"css",children:`.image-box {
+  width: 500px;
+  height: 400px;
+  text-align: center;
+  line-height: 398px;
+}
+
+.image {
+  max-width: 100%;
+  max-height: 100%;
+  vertical-align: middle;
+}`})]})};function h(e){return(0,s.jsxs)(t.Fragment,{children:[(0,s.jsxs)(a(),{children:[(0,s.jsx)("title",{children:"使用css让图片自适应居中"}),(0,s.jsx)("meta",{name:"keywords",content:"前端, css, 图片自适应, 图片居中"}),(0,s.jsx)("meta",{name:"description",content:"使用css让图片自适应居中"})]}),(0,s.jsx)(x,{})]})}},99388:function(e,i,n){"use strict";var s=n(85893),t=n(73935),c=n(45697),a=n.n(c),r=n(39332),l=n(81860),o=n(10932),d=n(75162),m=n(6171),u=n(76467),g=n(63791),p=n.n(g);let{BackTop:x}=l.default;function h(e){let i=(0,r.useRouter)();return[(0,s.jsxs)("div",{className:p().main,children:[e.goBack?(0,s.jsx)("div",{className:"mb-[16px] text-right",children:(0,s.jsxs)(o.ZP,{onClick:function(e){i.push("/")},children:[(0,s.jsx)(m.Z,{className:"mr-[6px]"}),"返回"]})}):null,e?.children]},"main"),e?.goToTop?(0,s.jsx)(u.default,{children:"object"==typeof document?(0,t.createPortal)((0,s.jsx)(x,{type:"primary",icon:(0,s.jsx)(d.Z,{}),title:"返回顶部",tabIndex:0,role:"button","aria-label":"返回顶部"}),document.body):null},"backTop"):null]}h.propTypes={children:a().node,goToTop:a().bool,goBack:a().bool},i.Z=h},30673:function(e,i,n){"use strict";var s=n(85893),t=n(45697),c=n.n(t),a=n(99388);function r(e){return(0,s.jsx)(a.Z,{goToTop:!0,goBack:!0,children:e.children})}r.propTypes={children:c().node},i.Z=r},63791:function(e){e.exports={main:"main_main__IZXSv",goToTop:"main_goToTop__Py8c8"}},98642:function(e){e.exports={picture:"CSSImageCentered_picture__ys_76",image:"CSSImageCentered_image__oniy6"}}},function(e){e.O(0,[932,416,2888,9774,179],function(){return e(e.s=2742)}),_N_E=e.O()}]);
