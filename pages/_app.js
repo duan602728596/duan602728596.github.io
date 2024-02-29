2c5d7e4258186c36bde84d3369bc9d8cad45fcb7
B.B.K.K.B.K.K <duanhaochen@126.com>
2024-02-29 15:40:34 +0800

    refactor: 修改MDXProvider的引入方式

--- pages/_app.js
+++ pages/_app.js
@@ -2,38 +2,14 @@ import { ConfigProvider, Typography } from 'antd';
 import 'antd/dist/reset.css';
 import zhCN from 'antd/locale/zh_CN';
 import { geekblue } from '@ant-design/colors';
-import { MDXProvider } from '@mdx-js/react';
-import MdxCodeHighLight from '../src/components/HighLight/MdxCodeHighLight';
 import Live2dSuspense from '../src/components/Live2d/Live2dSuspense';
 import Nav from '../src/components/Nav/Nav';
+import Mdx from '../src/components/Mdx/Mdx';
 import '../src/utils/Accessibility';
 import '../src/Accessibility.sass';
 import '../src/tailwindcss.css';
 
 const { Title, Paragraph } = Typography;
-
-/**
- * mdx h3组件
- * @param { React.ReactNode } props.children
- */
-function MdxH3(props) {
-  return <Title level={ 3 } id={ props.children }>{ props.children }</Title>;
-}
-
-/**
- * mdx h4组件
- * @param { React.ReactNode } props.children
- */
-function MdxH4(props) {
-  return <Title level={ 4 }>{ props.children }</Title>;
-}
-
-const MDXComponents = {
-  code: MdxCodeHighLight,
-  p: Paragraph,
-  h3: MdxH3,
-  h4: MdxH4
-};
 
 export default function App(props) {
   const { Component, pageProps } = props;
@@ -41,9 +17,9 @@ export default function App(props) {
   return (
     <ConfigProvider locale={ zhCN } theme={{ token: { colorPrimary: geekblue.primary } }}>
       <Nav />
-      <MDXProvider components={ MDXComponents }>
+      <Mdx>
         <Component { ...pageProps } />
-      </MDXProvider>
+      </Mdx>
       <Live2dSuspense />
     </ConfigProvider>
   );
